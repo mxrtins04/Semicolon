f30be3da680fa7bacad7eab3da8b8770219724f2
Martins <olatunosunmartins14@gmail.com>
2025-09-29 09:37:13 +0100

    anf

--- Python/Chapter 3/three_point_six (1).py
+++
@@ -1,9 +0,0 @@
-input("What is your problem? ")
-
-diagnosis = input("Have you had this problem before?(yes/no)")
-
-if diagnosis == yes:
-	print("Well you have it again")
-
-else:
-	print("Well you have it now")
--- Python/Chapter 3/three_point_ten (1).py
+++
@@ -1,9 +0,0 @@
-principal = 5000.0
-
-interest = 0.07
-
-amount = principal
-
-for year in range(1, 31):
-	amount = amount * 1 + interest
-	print("In year " ,year, "the amount is: " ,amount)
