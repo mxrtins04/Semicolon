88319578bc5fa225f5ad8be893f3a85542b83912
Martins <olatunosunmartins14@gmail.com>
2025-10-10 06:48:15 +0100

    mr ebuka's task 10-9-2025

---
+++ Python/Home_tasks/10-09-2025/functions.py
@@ -0,0 +1,63 @@
+class home_snack:
+	def get_length_of_list(the_list):
+		length = 0
+		for item in the_list:
+			length += 1
+		return(length)
+
+	def even_index_value_sum(the_list):
+		sum = 0
+		for item in(the_list):
+			if( item % 2 == 0):
+				sum += 1
+		return(sum)
+
+	def odd_index_value_sum(the_list):
+		sum = 0
+		for item in(the_list):
+			if( item % 2 != 0):
+				sum += 1
+		return(sum)
+
+	def get_third_position_product(the_list):
+		product = 1
+		count = 0
+		length = len(the_list)
+		for item in the_list:
+			count += 1
+			if( count % 3 == 0 ):
+				product *= item
+		return(product)
+
+	def get_average_of_list(the_list):
+		sum = 0
+		count = 0
+		for item in the_list:
+			count += 1
+			sum += the_list[item]
+
+		average = sum / count
+			
+	def get_largest_in_list(the_list):
+		highest = 0
+		for item in the_list:
+			if the_list[item] > highest:
+				highest = the_list[item]
+		return(highest)
+
+	def construct_list(the_list):
+		for number in range(1, 16, 2):
+			the_list.append(number)
+		return(the_list)
+	def get_third_element(the_list):
+		third_element = the_list[2]
+		return third_element
+
+	def get_sum_of_first_middlea_and_last_elements(the_list):
+		middle_index = (len(the_list)) - 1
+		fle_element = the_list[middle_index]
+	
+	
+
+			
+		
---
+++ Python/Home_tasks/10-09-2025/random_list.py
@@ -0,0 +1,12 @@
+import random
+
+
+
+random_list = []
+
+for index in range(10):
+	random_number = random.randint(1, 50)
+	random_list.append(random_number)
+
+print(random_list)
+	
