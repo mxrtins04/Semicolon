a87e259a74d4216e5563b4570fb6cf9288677935
Martins <olatunosunmartins14@gmail.com>
2025-10-17 12:53:02 +0100

    new task

---
+++ JS/Class_tasks(10-17-25)/EncrypyData.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class EncryptNumber{
+	public static void main(String [] args){
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input the 4 digit number you want to encrypt: ");
+		int userInput = input.nextInt();
+		
+		int [] number = new int[4];
+		int count = 0;
+		int divisor = 1000;
+		while(divisor >= 1){
+			
+			number[count] = userInput / divisor;
+			count++;
+			divisor *= 0.1;
+			}
+		
+		for( int index = 0; index < number.length; index++ ){
+			number[index] = (7 + number[index]) % 10;
+			}
+		int digit3Holder = number[2];
+		int digit1Holder = number[0];
+		int digit4Holder = number[3];
+		int digit2Holder = number[1];
+
+		number[0] = digit3Holder;
+		number[2] = digit1Holder;
+		number[1] = digit4Holder;
+		number[3] = digit2Holder;
+	
+		for( int index = 0; index < number.length; index++ ){
+			System.out.print(number[index]);}
+	}}
