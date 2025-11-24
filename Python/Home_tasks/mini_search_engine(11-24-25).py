288897c3a23cecd5f7349c557bf00b9f09ac9a8d
Martins <olatunbosunmartins14@gmail.com>
2025-11-24 05:37:29 +0100

    mini_search_engine

--- Python/Home_tasks/mini_search_engine(11-24-25).py
+++ Python/Home_tasks/mini_search_engine(11-24-25).py
@@ -1,1 +1,11 @@
-g
+def showMatches(input, keyword):
+    lowercase_input = input.lower()
+    lowercase_keyword = keyword.lower()
+    number_of_matches = 0
+    match_location = ()
+    for word in input:
+        index = 0
+        if  word == lowercase_keyword:
+            number_of_matches += 1
+            match_location += (index,)
+    return match_location
