64512142fc56778edd2ceaa98068d120a2ac0679
Martins <olatunbosunmartins14@gmail.com>
2025-11-27 12:07:12 +0100

    ...

--- Python/Home_tasks/Motorcycle/test.py
+++ Python/Home_tasks/Motorcycle/test.py
@@ -3,26 +3,43 @@ import unittest
 from functions import *
 class TestMotorcycle(unittest.TestCase):
 
+    def setUp(self):
+        state = {
+            "bikeState": False,
+            "speed": 0,
+            "gear": 1,
+            "maxGear": 4
+        }
+
+    def test_that_the_bike_is_off_by_default(self):
+        self.assertFalse(get_bike_state())
+
     def test_bike_starts(self):
         start_bike()
         self.assertTrue(get_bike_state())
 
-    def test_bike_stops(self):
-        start_bike()  # ensure it's on
+    def test_bike_can_be_switched_off_after_starting(self):
+        start_bike()
         turn_off_bike()
         self.assertFalse(get_bike_state())
-        self.assertEqual(get_speed(), 0)
 
     def test_acceleration(self):
         start_bike()
         accelerate()
         self.assertEqual(get_speed(), 1)
+    def test_speed_is_zero_when_bike_is_off(self):
+        start_bike()
+        accelerate()
+        accelerate()
+        turn_off_bike()
+        self.assertEqual(get_speed(), 0)
 
     def test_brake(self):
         start_bike()
         accelerate()
+        accelerate()
         brake()
-        self.assertEqual(get_speed(), 0)
+        self.assertEqual(get_speed(),  1)
 
 
     def test_accelerate_when_off(self):
