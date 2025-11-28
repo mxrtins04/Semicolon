8a14737bccd79eb7444b48daf651e481e86971cb
Martins <olatunbosunmartins14@gmail.com>
2025-11-28 01:23:01 +0100

    Motorcycle js completed

--- JS/Home_Tasks/Motorcycle/Test.test.js
+++ JS/Home_Tasks/Motorcycle/Test.test.js
@@ -1,39 +1,138 @@
-const  { setGear, accelerate, startBike, switchOffBike, getSpeed} = require('./motorcycle')
-let bike = {bikeState: false,
-             speed: 0,
-              gear: 1
-            }
+const {
+	startBike,
+	switchOffBike,
+	getBikeState,
+	getSpeed,
+	getGear,
+	setSpeed,
+	accelerate,
+	brake,
+	increaseGear,
+	reduceGear
+} = require('./motorcycle');
+
+let bike = {
+	bikeState: false,
+	speed: 0,
+	gear: 1
+};
+
 beforeEach(() => {
-    bike.bikeState =false,
-    bike.speed = 0,
-    bike.gear = 1
-            
+	bike.bikeState = false;
+	bike.speed = 0;
+	bike.gear = 1;
 });
 
-test ("test bike is off by default", () =>{
-    expect(bike.bikeState).toBeFalsy();
+test("test bike is off by default", () => {
+	expect(getBikeState(bike)).toBeFalsy();
 });
 
-test ("test bike can be turned on", () =>{
-    startBike(bike)
-    expect(bike.bikeState).toBeFalsy();
-})
+test("test bike can be turned on", () => {
+	startBike(bike);
+	expect(getBikeState(bike)).toBeTruthy();
+});
 
-test ("test that bike can be turned off when its on", () =>{
-    startBike(bike);
-    switchOffBike(bike);
-    expect(bike.bikeState).toBeFalsy();
-})
+test("test bike can be turned off", () => {
+	startBike(bike);
+	switchOffBike(bike);
+	expect(getBikeState(bike)).toBeFalsy();
+});
 
-test ("test that initial speed of bike is 0", () =>{
-    let speed = getSpeed(bike);
-    expect(speed).toBe(0);
-})
+test("test initial gear is one", () => {
+	expect(getGear(bike)).toBe(1);
+});
 
-test ("test that accelerator increases speed", () =>{
-    accelerate(bike);
-    speed = getSpeed(bike);
-    expect(speed).toBe(1);
-})
+test("test initial speed is zero", () => {
+	expect(getSpeed(bike)).toBe(0);
+});
 
-test ("test that a")
+test("test accelerator increases speed by 1 in gear 1", () => {
+	startBike(bike);
+	accelerate(bike);
+	expect(getSpeed(bike)).toBe(1);
+});
+
+test("test accelerator can be called multiple times", () => {
+	startBike(bike);
+	accelerate(bike);
+	accelerate(bike);
+	accelerate(bike);
+	expect(getSpeed(bike)).toBe(3);
+});
+
+test("test acceleration depends on gear", () => {
+	startBike(bike);
+	setSpeed(bike, 25);
+	accelerate(bike);
+	expect(getSpeed(bike)).toBe(27);
+
+	setSpeed(bike, 35);
+	accelerate(bike);
+	expect(getSpeed(bike)).toBe(38);
+});
+
+test("test bike cannot accelerate when off", () => {
+	accelerate(bike);
+	expect(getSpeed(bike)).toBe(0);
+});
+
+test("test setGear works automatically based on speed", () => {
+	startBike(bike);
+	setSpeed(bike, 19);
+	accelerate(bike);
+	accelerate(bike);
+	expect(getGear(bike)).toBe(2);
+
+	setSpeed(bike, 30);
+	accelerate(bike);
+	expect(getGear(bike)).toBe(3);
+});
+
+test("test brake reduces speed", () => {
+	setSpeed(bike, 10);
+	brake(bike);
+	expect(getSpeed(bike)).toBe(9);
+});
+
+test("test rate of deceleration increases with gear", () => {
+	setSpeed(bike, 25);
+	brake(bike);
+	expect(getSpeed(bike)).toBe(23);
+});
+
+test("test speed never goes below zero", () => {
+	brake(bike);
+	brake(bike);
+	expect(getSpeed(bike)).toBe(0);
+});
+
+test("test speed resets to zero when bike is turned off", () => {
+	startBike(bike);
+	setSpeed(bike, 20);
+	switchOffBike(bike);
+	expect(getSpeed(bike)).toBe(0);
+});
+
+test("test increaseGear works", () => {
+	increaseGear(bike);
+	expect(getGear(bike)).toBe(2);
+});
+
+test("test increaseGear cannot exceed 4", () => {
+	setSpeed(bike, 50);
+	expect(getGear(bike)).toBe(4);
+	increaseGear(bike);
+	expect(getGear(bike)).toBe(4);
+});
+
+test("test reduceGear works", () => {
+	setSpeed(bike, 35);
+	expect(getGear(bike)).toBe(3);
+	reduceGear(bike);
+	expect(getGear(bike)).toBe(2);
+});
+
+test("test reduceGear never goes below 1", () => {
+	reduceGear(bike);
+	expect(getGear(bike)).toBe(1);
+});
--- JS/Home_Tasks/Motorcycle/motorcycle.js
+++ JS/Home_Tasks/Motorcycle/motorcycle.js
@@ -1,43 +1,105 @@
-let bike = {bikeState: false,
-             speed: 0,
-              gear: 1
-            }
-
-function startBike(bike){
-    let bikeState = bike.bikeState
-    bikeState = true;
-
+function startBike(bike) {
+	bike.bikeState = true;
 }
 
-function switchOffBike(bike){
-    let bikeState = bike.bikeState;
-    bikeState = false;
+function switchOffBike(bike) {
+	bike.bikeState = false;
+	bike.speed = 0;
+	bike.gear = 1;
 }
 
-function getSpeed(bike){
-    let speed = bike.speed;
-    return speed;
+function getBikeState(bike) {
+	return bike.bikeState;
 }
 
-function setGear(){
-    if (speed > 0 && speed <=20){
-        bike["gear"] = 1;
-    }
-    else if (speed >= 21 && speed <=30){
-        bike["gear"] = 2;
-    }
-    else if (speed >= 31 && speed <=40){
-        bike["gear"] = 3;
-    }
-    else if( speed > 40)
-        bike["gear"] = 4;
-
+function getSpeed(bike) {
+	return bike.speed;
 }
 
-function accelerate(bike){
-    bike.speed += 1;
+function getGear(bike) {
+	return bike.gear;
 }
-module.exports = {setGear, startBike, switchOffBike, getSpeed, accelerate}
 
+function setSpeed(bike, newSpeed) {
+	if (newSpeed < 0) {
+		bike.speed = 0;
+	} else {
+		bike.speed = newSpeed;
+	}
+	updateGear(bike);
+}
 
+function updateGear(bike) {
+	let currentSpeed = bike.speed;
 
+	if (currentSpeed >= 0 && currentSpeed <= 20) {
+		bike.gear = 1;
+	} else if (currentSpeed >= 21 && currentSpeed <= 30) {
+		bike.gear = 2;
+	} else if (currentSpeed >= 31 && currentSpeed <= 40) {
+		bike.gear = 3;
+	} else if (currentSpeed > 40) {
+		bike.gear = 4;
+	}
+}
+
+function accelerate(bike) {
+	if (bike.bikeState === false) {
+		return;
+	}
+
+	if (bike.gear === 1) {
+		bike.speed = bike.speed + 1;
+	} else if (bike.gear === 2) {
+		bike.speed = bike.speed + 2;
+	} else if (bike.gear === 3) {
+		bike.speed = bike.speed + 3;
+	} else if (bike.gear === 4) {
+		bike.speed = bike.speed + 4;
+	}
+
+	updateGear(bike);
+}
+
+function brake(bike) {
+	if (bike.gear === 1) {
+		bike.speed = bike.speed - 1;
+	} else if (bike.gear === 2) {
+		bike.speed = bike.speed - 2;
+	} else if (bike.gear === 3) {
+		bike.speed = bike.speed - 3;
+	} else if (bike.gear === 4) {
+		bike.speed = bike.speed - 4;
+	}
+
+	if (bike.speed < 0) {
+		bike.speed = 0;
+	}
+
+	updateGear(bike);
+}
+
+function increaseGear(bike) {
+	if (bike.gear < 4) {
+		bike.gear = bike.gear + 1;
+	}
+}
+
+function reduceGear(bike) {
+	if (bike.gear > 1) {
+		bike.gear = bike.gear - 1;
+	}
+}
+
+module.exports = {
+	startBike,
+	switchOffBike,
+	getBikeState,
+	getSpeed,
+	getGear,
+	setSpeed,
+	accelerate,
+	brake,
+	increaseGear,
+	reduceGear
+};
